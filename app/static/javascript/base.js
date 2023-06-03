69a4ae23b7944c5af88267de95533b1e9b47c296
ruyi <tan_ruyi@hotmail.com>
2023-06-03 16:15:13 +0800

    - rotation of icon for navbar done

--- app/static/javascript/base.js
+++ app/static/javascript/base.js
@@ -11,16 +11,35 @@ window.addEventListener("load", function () {
 	const comparisonNavItem = document.getElementById("nav-item-comparison");
 	const changelogNavItem = document.getElementById("nav-item-changelog");
 	const navDropdownAlgorithms = document.getElementById("nav-dropdown-algorithms");
+	const navDropdownComparison = document.getElementById("nav-dropdown-comparison");
+
+	const algorithmsIcon = document.getElementById("nav-item-algorithms-icon");
+	const comparisonIcon = document.getElementById("nav-item-comparison-icon");
 
 	/* ===========================================
     FUNCTIONS
     =========================================== */
 	function toggleAlgorithmsDropdown() {
-		console.log("hi");
 		if (navDropdownAlgorithms.style.display === "none") {
+			algorithmsNavItem.style.color = "var(--hot-pink)";
 			navDropdownAlgorithms.style.display = "flex";
+			algorithmsIcon.classList.add("cs-navbar-icon-rotate");
 		} else {
+			algorithmsNavItem.style.color = "var(--black)";
 			navDropdownAlgorithms.style.display = "none";
+			algorithmsIcon.classList.remove("cs-navbar-icon-rotate");
+		}
+	}
+
+	function toggleComparisonDropdown() {
+		if (navDropdownComparison.style.display === "none") {
+			comparisonNavItem.style.color = "var(--hot-pink)";
+			navDropdownComparison.style.display = "flex";
+			comparisonIcon.classList.add("cs-navbar-icon-rotate");
+		} else {
+			comparisonNavItem.style.color = "var(--black)";
+			navDropdownComparison.style.display = "none";
+			comparisonIcon.classList.remove("cs-navbar-icon-rotate");
 		}
 	}
 
@@ -29,5 +48,9 @@ window.addEventListener("load", function () {
     =========================================== */
 	algorithmsNavItem.addEventListener("click", function () {
 		toggleAlgorithmsDropdown();
+	});
+
+	comparisonNavItem.addEventListener("click", function () {
+		toggleComparisonDropdown();
 	});
 });
