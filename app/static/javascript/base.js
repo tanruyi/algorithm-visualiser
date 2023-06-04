f346167a4ce8a40008bcb8cd56b557b01c6c4926
ruyi <tan_ruyi@hotmail.com>
2023-06-04 15:17:42 +0800

    - changelog dropdown done

--- app/static/javascript/base.js
+++ app/static/javascript/base.js
@@ -7,24 +7,34 @@ window.addEventListener("load", function () {
     DOM ELEMENTS
     =========================================== */
 
+	// nav buttons
 	const algorithmsNavItem = document.getElementById("nav-item-algorithms");
 	const comparisonNavItem = document.getElementById("nav-item-comparison");
 	const changelogNavItem = document.getElementById("nav-item-changelog");
+
+	// nav dropdowns
 	const navDropdownAlgorithms = document.getElementById("nav-dropdown-algorithms");
 	const navDropdownComparison = document.getElementById("nav-dropdown-comparison");
+	const navDropdownChangelog = document.getElementById("nav-dropdown-changelog");
 
+	// nav button icons
 	const algorithmsIcon = document.getElementById("nav-item-algorithms-icon");
 	const comparisonIcon = document.getElementById("nav-item-comparison-icon");
+	const changelogIcon = document.getElementById("nav-item-changelog-icon");
 
 	/* ===========================================
     FUNCTIONS
     =========================================== */
 	function toggleAlgorithmsDropdown() {
 		if (navDropdownAlgorithms.style.display === "none") {
+			console.log("algo on ");
+
 			algorithmsNavItem.style.color = "var(--hot-pink)";
 			navDropdownAlgorithms.style.display = "flex";
 			algorithmsIcon.classList.add("cs-navbar-icon-rotate");
 		} else {
+			console.log("algo off");
+
 			algorithmsNavItem.style.color = "var(--black)";
 			navDropdownAlgorithms.style.display = "none";
 			algorithmsIcon.classList.remove("cs-navbar-icon-rotate");
@@ -32,6 +42,8 @@ window.addEventListener("load", function () {
 	}
 
 	function toggleComparisonDropdown() {
+		console.log("comp");
+
 		if (navDropdownComparison.style.display === "none") {
 			comparisonNavItem.style.color = "var(--hot-pink)";
 			navDropdownComparison.style.display = "flex";
@@ -43,14 +55,37 @@ window.addEventListener("load", function () {
 		}
 	}
 
+	function toggleChangelogDropdown() {
+		console.log("change");
+
+		if (navDropdownChangelog.style.display === "none") {
+			changelogNavItem.style.color = "var(--hot-pink)";
+			navDropdownChangelog.style.display = "block";
+			changelogIcon.classList.add("cs-navbar-icon-rotate");
+		} else {
+			changelogNavItem.style.color = "var(--black)";
+			navDropdownChangelog.style.display = "none";
+			changelogIcon.classList.remove("cs-navbar-icon-rotate");
+		}
+	}
+
 	/* ===========================================
     EVENT LISTENERS
     =========================================== */
-	algorithmsNavItem.addEventListener("click", function () {
+	algorithmsNavItem.addEventListener("click", function (e) {
+		console.log(e.target);
 		toggleAlgorithmsDropdown();
 	});
 
-	comparisonNavItem.addEventListener("click", function () {
+	comparisonNavItem.addEventListener("click", function (e) {
+		console.log(e.target);
+
 		toggleComparisonDropdown();
+	});
+
+	changelogNavItem.addEventListener("click", function (e) {
+		console.log(e.target);
+
+		toggleChangelogDropdown();
 	});
 });
